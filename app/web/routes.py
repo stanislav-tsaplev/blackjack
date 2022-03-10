ea68f117816119a42f2a00402f72821a40217cc7
Станислав Цаплев <stanislav.tsaplev@gmail.com>
2022-03-10 16:17:18 +0300

    remove excessive files

--- app/web/routes.py
+++ app/web/routes.py
@@ -3,7 +3,5 @@ from aiohttp.web_app import Application
 
 def setup_routes(app: Application):
     from app.web.admin.routes import setup_routes as admin_setup_routes
-    from app.web.bot.routes import setup_routes as bot_setup_routes
 
     admin_setup_routes(app)
-    bot_setup_routes(app)
