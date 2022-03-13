adca8d9034a2ab64b28f6feeb43f4112f1c7a8d5
Станислав Цаплев <stanislav.tsaplev@gmail.com>
2022-03-13 20:43:01 +0300

    add handling aiohttp.ClientConnectionError

--- app/vk_api/accessor.py
+++ app/vk_api/accessor.py
@@ -1,7 +1,7 @@
 import random
 from typing import List, Optional, TYPE_CHECKING
 
-from aiohttp import TCPConnector
+from aiohttp import ClientConnectionError, TCPConnector
 from aiohttp.client import ClientSession
 
 from app.db.base.accessor import BaseAccessor
@@ -51,7 +51,7 @@ class VkApiAccessor(BaseAccessor):
         if self.session:
             await self.session.close()   
 
-    async def _get_long_poll_server(self):
+    async def _get_long_poll_server(self) -> None:
         query_url = build_query_url(
             api_path=VK_API_PATH,
             api_method="groups.getLongPollServer",
@@ -61,17 +61,23 @@ class VkApiAccessor(BaseAccessor):
                 "lp_version": 3,
             },
         )
-        async with self.session.get(query_url) as response:
-            if response.status != 200:
-                error_message = f"error during getting long poll server: {response}"
-                self.logger.error(error_message)
-                raise ConnectionError(error_message)
-            
-            json = await response.json()
-            data = json["response"]
-            self.logger.info(data)
+        try:
+            async with self.session.get(query_url) as response:
+                if response.status != 200:
+                    error_message = f"error during getting long poll server: {response}"
+                    self.logger.error(error_message)
+                    raise ConnectionError(error_message)
+                
+                json = await response.json()
+                data = json["response"]
+                self.logger.info(data)
 
-            return data["key"], data["server"], data["ts"]
+                return data["key"], data["server"], data["ts"]
+        except ClientConnectionError as e:
+            self.logger.error(exc_info=e)
+            self.session = ClientSession(connector=TCPConnector(verify_ssl=False))
+            self._get_long_poll_server()
+
 
     async def poll(self) -> list[VkApiUpdate]:
         query_url = build_query_url(
@@ -86,46 +92,52 @@ class VkApiAccessor(BaseAccessor):
                 "version": 3,
             },
         )
-        async with self.session.get(query_url) as response:
-            json = await response.json()
-            self.logger.info(json)
+        try:
+            async with self.session.get(query_url) as response:
+                json = await response.json()
+                self.logger.info(json)
 
-            if "failed" not in json:
-                self.ts = json["ts"]
-                try:
-                    json_updates = json.get("updates", [])
-                    return [
-                        VkApiUpdate.from_dict(json_update)
-                        for json_update in json_updates
-                    ]
-                except Exception as e:
-                    self.logger.error("exception during update parsing", exc_info=e)
-            else:
-                self.logger.error("error during getting updates", exc_info=json)
-                error_code = json["failed"]
-                
-                # "{"failed":1,"ts":$new_ts} — 
-                # история событий устарела или была частично утеряна, 
-                # приложение может получать события далее, 
-                # используя новое значение ts из ответа.
-                if error_code == 1:
+                if "failed" not in json:
                     self.ts = json["ts"]
-
-                # {"failed":2} — истекло время действия ключа, 
-                #  нужно заново получить key методом messages.getLongPollServer.
-                # {"failed":3} — информация о пользователе утрачена, 
-                #  нужно запросить новые key и ts методом messages.getLongPollServer.
-                elif error_code in (2, 3):
                     try:
-                        self.key, self.server, self.ts = await self._get_long_poll_server()
+                        json_updates = json.get("updates", [])
+                        return [
+                            VkApiUpdate.from_dict(json_update)
+                            for json_update in json_updates
+                        ]
                     except Exception as e:
-                        self.logger.error(
-                            "exception during long poll service getting", 
-                            exc_info=e
-                        )
-                        await self.session.close()
-                
-                return self.poll()
+                        self.logger.error("exception during update parsing", exc_info=e)
+                else:
+                    self.logger.error("error during getting updates", exc_info=json)
+                    error_code = json["failed"]
+                    
+                    # "{"failed":1,"ts":$new_ts} — 
+                    # история событий устарела или была частично утеряна, 
+                    # приложение может получать события далее, 
+                    # используя новое значение ts из ответа.
+                    if error_code == 1:
+                        self.ts = json["ts"]
+
+                    # {"failed":2} — истекло время действия ключа, 
+                    #  нужно заново получить key методом messages.getLongPollServer.
+                    # {"failed":3} — информация о пользователе утрачена, 
+                    #  нужно запросить новые key и ts методом messages.getLongPollServer.
+                    elif error_code in (2, 3):
+                        try:
+                            self.key, self.server, self.ts = await self._get_long_poll_server()
+                        except Exception as e:
+                            self.logger.error(
+                                "exception during long poll service getting", 
+                                exc_info=e
+                            )
+                            await self.session.close()
+                                                
+                    return self.poll()
+        
+        except ClientConnectionError as e:
+            self.logger.error(exc_info=e)
+            self.session = ClientSession(connector=TCPConnector(verify_ssl=False))
+            return self.poll()
 
     async def send_message(self, peer_id: int, text: str) -> int:
         query_url = build_query_url(
@@ -138,17 +150,22 @@ class VkApiAccessor(BaseAccessor):
                 "access_token": self.app.config.bot.token,
             },
         )
-        async with self.session.get(query_url) as response:
-            json = await response.json()
-            self.logger.info(json)
+        try:
+            async with self.session.get(query_url) as response:
+                json = await response.json()
+                self.logger.info(json)
 
-            if "error" not in json:
-                sent_message_id = int(json["response"])
-                self.logger.info(f"message sent with id: {sent_message_id}")
-                return sent_message_id
-            else:
-                self.logger.error("error during message sending")
-                return None
+                if "error" not in json:
+                    sent_message_id = int(json["response"])
+                    self.logger.info(f"message sent with id: {sent_message_id}")
+                    return sent_message_id
+                else:
+                    self.logger.error("error during message sending")
+                    return None
+        except ClientConnectionError as e:
+            self.logger.error(exc_info=e)
+            self.session = ClientSession(connector=TCPConnector(verify_ssl=False))
+            self.send_message(peer_id, text)
 
     async def send_message_with_keyboard(self, peer_id: int, text: str, keyboard: str) -> int:
         query_url = build_query_url(
@@ -162,17 +179,22 @@ class VkApiAccessor(BaseAccessor):
                 "access_token": self.app.config.bot.token,
             },
         )
-        async with self.session.get(query_url) as response:
-            json = await response.json()
-            self.logger.info(json)
+        try:
+            async with self.session.get(query_url) as response:
+                json = await response.json()
+                self.logger.info(json)
 
-            if "error" not in json:
-                sent_message_id = int(json["response"])
-                self.logger.info(f"message sent with id: {sent_message_id}")
-                return sent_message_id
-            else:
-                self.logger.error("error during message sending")
+                if "error" not in json:
+                    sent_message_id = int(json["response"])
+                    self.logger.info(f"message sent with id: {sent_message_id}")
+                    return sent_message_id
+                else:
+                    self.logger.error("error during message sending")
                 return None
+        except ClientConnectionError as e:
+            self.logger.error(exc_info=e)
+            self.session = ClientSession(connector=TCPConnector(verify_ssl=False))
+            self.send_message_with_keyboard(peer_id, text, keyboard)
 
     async def update_message(self, peer_id: int, message_id: int, text: str) -> None:
         query_url = build_query_url(
@@ -185,14 +207,19 @@ class VkApiAccessor(BaseAccessor):
                 "access_token": self.app.config.bot.token,
             },
         )
-        async with self.session.get(query_url) as response:
-            json = await response.json()
-            self.logger.info(json)
+        try:
+            async with self.session.get(query_url) as response:
+                json = await response.json()
+                self.logger.info(json)
 
-            if "error" not in json:
-                self.logger.info(f"message with id: {message_id} updated")
-            else:
-                self.logger.error("error during message sending")
+                if "error" not in json:
+                    self.logger.info(f"message with id: {message_id} updated")
+                else:
+                    self.logger.error("error during message sending")
+        except ClientConnectionError as e:
+            self.logger.error(exc_info=e)
+            self.session = ClientSession(connector=TCPConnector(verify_ssl=False))
+            self.update_message(peer_id, message_id, text)
 
 
     async def get_active_member_profiles(self, peer_id: int
@@ -206,18 +233,23 @@ class VkApiAccessor(BaseAccessor):
                 "access_token": self.app.config.bot.token,
             },
         )
-        async with self.session.get(query_url) as response:
-            json = await response.json()
+        try:
+            async with self.session.get(query_url) as response:
+                json = await response.json()
 
-            if "error" not in json:
-                chat_members_info = VkApiMembersResponse.from_dict(json["response"])
-                active_chat_member_profiles = [
-                    profile for profile 
-                    in chat_members_info.profiles 
-                    if profile.online
-                ]
+                if "error" not in json:
+                    chat_members_info = VkApiMembersResponse.from_dict(json["response"])
+                    active_chat_member_profiles = [
+                        profile for profile 
+                        in chat_members_info.profiles 
+                        if profile.online
+                    ]
 
-                self.logger.info(f"active members in chat {peer_id}: {active_chat_member_profiles}")
-                return active_chat_member_profiles
-            else:
-                self.logger.error(f"error during getting chat members: {json}")
+                    self.logger.info(f"active members in chat {peer_id}: {active_chat_member_profiles}")
+                    return active_chat_member_profiles
+                else:
+                    self.logger.error(f"error during getting chat members: {json}")
+        except ClientConnectionError as e:
+            self.logger.error(exc_info=e)
+            self.session = ClientSession(connector=TCPConnector(verify_ssl=False))
+            return self.get_active_member_profiles(peer_id)
