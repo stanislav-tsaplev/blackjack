810c965c2fef10cf0c3599ceda9ee23b22ba612d
Станислав Цаплев <stanislav.tsaplev@gmail.com>
2022-03-13 21:21:59 +0300

    fix bug with calculation score with ace equals 11

--- app/bot/utils/player_hand.py
+++ app/bot/utils/player_hand.py
@@ -7,16 +7,18 @@ class PlayerHand:
     def __init__(self, cards: List[Card]):
         self.cards = cards
         self._pre_score = sum(min(card.rank.value, 10) for card in self.cards)
-        self._plus_score = 0 \
-            if (self._pre_score > 11 or CardRank.Ace not in self.cards) \
-            else 10
+        self._plus_score = 10 if (
+                self._pre_score <= 11 and 
+                any(card.rank == CardRank.Ace for card in self.cards)
+            ) else 0
 
     def take(self, card: Card):
         self.cards.append(card)
         self._pre_score += min(card.rank.value, 10)
-        self._plus_score = 0 \
-            if (self._pre_score > 11 or CardRank.Ace not in self.cards) \
-            else 10
+        self._plus_score = 10 if (
+                self._pre_score <= 11 and 
+                any(card.rank == CardRank.Ace for card in self.cards)
+            ) else 0
 
     @property
     def score(self) -> int:
