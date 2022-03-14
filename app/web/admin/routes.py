3802151bbcc544a29597ac5f9aea9421caba2ae4
Станислав Цаплев <stanislav.tsaplev@gmail.com>
2022-03-14 13:18:57 +0300

    split admin views into separate files in newly created folder

--- app/web/admin/routes.py
+++ app/web/admin/routes.py
@@ -1,14 +1,18 @@
 from typing import TYPE_CHECKING
 
-from app.web.admin.views import (
+from app.web.admin.views.login import (
+    AdminIndexView,
     AdminLoginView, 
-    AdminCurrentView
+    AdminCurrentView,
 )
+# from app.web.admin.views.games import AdminInfoGamesView
 
 if TYPE_CHECKING:
     from app.web.app import Application
 
 
 def setup_routes(app: "Application"):
+    app.router.add_view("/admin", AdminIndexView)
     app.router.add_view("/admin.login", AdminLoginView)
     app.router.add_view("/admin.current", AdminCurrentView)
+    # app.router.add_view("/admin.info.games", AdminInfoGamesView)
